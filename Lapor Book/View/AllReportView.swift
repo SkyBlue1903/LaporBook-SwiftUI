4d63cdec0375263c50b3dd40a02f25c1f425b74f
Erlangga Anugrah Arifin <angga1433@gmail.com>
2023-12-29 23:18:31 +0700

    minor fix for UI

--- Lapor Book/View/AllReportView.swift
+++ Lapor Book/View/AllReportView.swift
@@ -18,54 +18,32 @@ final class AllReportViewModel: ObservableObject {
 
 struct AllReportView: View {
   @Environment(\.colorScheme) var colorScheme
-  @State private var addReportSheet: Bool = false
   @StateObject private var viewModel = MyReportViewModel()
   
   let columnSize: [GridItem] = Array(repeating: GridItem(.flexible(), spacing: 10), count: 2)
   var body: some View {
-    if addReportSheet {
-      Task {
-        do {
-          try await viewModel.loadReports()
+    NavigationStack {
+      ScrollView(.vertical){
+        LazyVGrid(columns: columnSize) {
+          ForEach(viewModel.data, id: \.self) { each in
+            NavigationLink(destination: ReportDetailView(data: each)) {
+              ReportCard2View(data: each)
+            }
+          }
         }
+        .padding(.horizontal)
       }
+      .onAppear(perform: {
+        Task {
+          do {
+            try await viewModel.loadReports()
+          } catch {
+            print("Error fetching all data when view appear:", error.localizedDescription)
+          }
+        }
+      })
+      .navigationTitle("Semua Laporan")
     }
-    return AnyView(
-      NavigationStack {
-        ScrollView(.vertical){
-          LazyVGrid(columns: columnSize) {
-            ForEach(viewModel.data, id: \.self) { each in
-              NavigationLink(destination: ReportDetailView(data: each)) {
-                ReportCard2View(data: each)
-              }
-            }
-          }
-          .padding(.horizontal)
-        }
-        .toolbar(content: {
-          ToolbarItem(placement: .topBarTrailing) {
-            Button(action: {
-              self.addReportSheet.toggle()
-            }) {
-              Image(systemName: "plus")
-            }
-          }
-        })
-        .sheet(isPresented: $addReportSheet, content: {
-          AddReportView(isPresented: $addReportSheet)
-        })
-        .onAppear(perform: {
-          Task {
-            do {
-              try await viewModel.loadReports()
-            } catch {
-              print("Error fetching all data when view appear:", error.localizedDescription)
-            }
-          }
-        })
-        .navigationTitle("Semua Laporan")
-      }
-    )
   }
 }
 
